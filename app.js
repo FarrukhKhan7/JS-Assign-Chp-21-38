d910dfe21b0414fa2d004ffceb4b261ae52427c1
FarrukhKhan7 <58085304+FarrukhKhan7@users.noreply.github.com>
2020-07-07 23:06:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+////////================CHAPTER 21 STRING METHODS=====================///////////////////////////////////////////////////////////////////////
+
+
+// TASK 1
+//  var firstname = prompt("Enter first name: ");
+//  var lastname = prompt("Enter last name: ");
+//  var fullName = firstname+" "+lastname;
+//  alert("Welcome "+fullName);
